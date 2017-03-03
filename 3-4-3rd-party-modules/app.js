fe0b8efd3d8bd3616e95660b0a5a3354e611d8d0
gregwhitaker <gwhitake@gmail.com>
2017-03-03 11:49:11 -0800

    Added 3rd party modules lesson

--- 3-4-3rd-party-modules/app.js
+++ 3-4-3rd-party-modules/app.js
@@ -2,7 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes.js');
+
+console.log(_.isString('test'));
+console.log(_.isString(true));
+
+console.log(_.uniq([2,1,2]));
 
 var result = notes.add(1, 2);
 console.log(result);
