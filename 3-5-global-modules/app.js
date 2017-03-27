ca0d0df9e9d638ce2f52c60c1ea834e32c34bdc0
gregwhitaker <gwhitake@gmail.com>
2017-03-27 13:16:08 -0700

    Added 3-5

---
+++ 3-5-global-modules/app.js
@@ -0,0 +1,9 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash')
+const notes = require('./notes.js');
+
+var result = _.uniq(['Mike']);
+console.log(result);
